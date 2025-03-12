f56f525fb064d0fc2325319b38cb5c5e6d5145ce
Yanze Song <yanzesong@rOot.local>
2025-03-12 17:02:00 -0600

    removed unnecessary files

--- model.py
+++ model.py
@@ -1,19 +1,14 @@
 import pandas as pd
 import numpy as np
-from data_old import clean_data
+from data import clean_data
 
 
-
-def linear_regression(density_infection_rate):
+def linear_regression(state_density_rate):
     '''
     Perform linear regression on the data
 
     Parameter:
-        - density_infection_rate (pd.dataframe):
-                        Population_Density     Infection_Rate
-            State_Name          ...                 ...
-                ...
-            State_Name          ...                 ...
+        - 
     '''
 
     pass
@@ -21,7 +16,7 @@ def linear_regression(density_infection_rate):
 
 
 if __name__ == "__main__":
-    density_infection_rate = clean_data()
-    linear_regression(density_infection_rate)
+    state_density_rate = clean_data()
+    linear_regression(state_density_rate)
 
     pass
